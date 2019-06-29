c88d0a5d5cf56b4d925ebb6b4883876a0507989c
jimmy <hae@qq.com>
2019-06-29 17:26:35 +0800

    test

---
+++ FlyingSwiftExtension/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FlyingSwiftExtension
+//
+//  Created by jun on 2019/6/29.
+//  Copyright © 2019 OCX.ai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("测试")
+    }
+}
+
